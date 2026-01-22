f5bef6fce2fa419a1c3a06ae8972c9799c646634
RangS-1 <rangga19sj@gmail.com>
2026-01-22 20:57:54 +0700

    nested if

--- C/9nested/main.c
+++ C/9nested/main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
-    //belom
+    float price = 10.00;
+    bool student = true;
+    bool senior = true;
+
+    if(student){
+        if(senior){
+            printf("You get a student discount 10%\n");
+            printf("You get a senior discount 20%\n");
+            price *= 0.7; //30% discount
+        }
+        else{
+            printf("You get a student discount 10%\n");
+            price *= 0.9; //10% discount
+        }
+    }
+    else{
+        if(senior){
+            printf("You get a senior discount 20%\n");
+            price *= 0.8;
+        }
+    }
+
+    printf("The price of a ticket is: $%.2f\n", price);
+    
+    return 0; 
 }
