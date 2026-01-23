98b690b9bf7746ca150152af50867727f3318cb8
RangS-1 <rangga19sj@gmail.com>
2026-01-23 10:24:19 +0700

    return

--- C/12return/main.c
+++ C/12return/main.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int square(int num){ //return function! int, double, bool, string, float
     return num * num; //if it not using return, the int result will not do anything
 }
-
+double cube(double num){
+    return num * num * num;
+}
+bool agecheck(int age){
+    if(age < 18){ //true
+        return true;
+    }
+    else{ //false
+        return false;
+    }
+}
 int main(){
     int x = square(2);
+    double y = cube(9);
+    int age = 21;
+    printf("%d\n", x);
+    printf("%.2f\n", y);
 
-    printf("%d", x);
-
+    if(agecheck(age)){ //true
+        printf("you must sign up");
+    }
+    else{ //false
+        printf("You are 18+");
+    }
     return 0;
 }
