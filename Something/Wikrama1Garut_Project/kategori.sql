b7449366cc9c901e643c32fc0c31a74569e66a68
RangS-1 <rangga19sj@gmail.com>
2026-03-12 16:56:50 +0700

    add kategori.sql and supplier.sql

---
+++ Something/Wikrama1Garut_Project/kategori.sql
@@ -0,0 +1,9 @@
+INSERT INTO kategori(
+    VALUES(NULL,"Mayonaise","Saus Tartar"),
+    (NULL,"Sauce","Saus Pedas"),
+    (NULL,"Vegetables","Sayuran Beku"),
+    (NULL,"Snack","Makanan Ringan"),
+    (NULL,"Burger","Daging Siap Pakai"),
+    (NULL,"Bird","Unggas"),
+    (NULL,"Pastry","Roti Beku")
+);
---
+++ Something/Wikrama1Garut_Project/supplier.sql
@@ -0,0 +1,9 @@
+INSERT INTO supplier(
+    VALUES(NULL,"Daffa","Wikrama","+12365","kanigara@gmail.com",""),
+    (NULL,"Wijdan","Rumah","+6298141","wijdun@gmail.com",""),
+    (NULL,"Aidil","Wikrama","+6432143","aidal@gmail.com",""),
+    (NULL,"Fachrezy","Guntur","+092132","archcuy@gmail.com",""),
+    (NULL,"Akhtar","Ausbildung","+5368748","akhtar@gmail.com",""),
+    (NULL,"Hasan","Wikrama","+6298141","hasnur@gmail.com",""),
+    (NULL,"Fazri","Rumah","+6298141","fajarsenja@gmail.com","")
+);
